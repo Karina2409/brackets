3a01d072f85beff46db3c52dda5ebdd238744d02
Karina2409 <karina.siardziuk@mail.ru>
2024-08-14 15:17:03 +0300

    feat: add tesk solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  let result = true;
+  let j = 0;
+  for (j; j < str.length; j++) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      let openBracket = bracketsConfig[i][0];
+      let closeBracket = bracketsConfig[i][1];
+      let bracket = str[j];
+      if (bracket === closeBracket && brackets.at(-1) === openBracket) {
+        brackets.pop();
+      }
+      else if (bracket === openBracket) {
+        brackets.push(bracket);
+      }
+      else if (bracket === closeBracket && brackets.at(-1) !== openBracket) {
+        return false;
+      }
+    }
+  }
+  if (brackets.length !== 0) {
+    return false;
+  }
+  return result;
 }
